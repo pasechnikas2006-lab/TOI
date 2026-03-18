c6d91d1b40eba1e83b488504164a0e61bf16f3fc
Diganazz_ <pasechnik.a.s.2006@gmail.com>
2026-03-18 21:26:52 +0300

    Update task 4.2.py

--- lab4/task 4.2.py
+++ lab4/task 4.2.py
@@ -1,12 +1,21 @@
-def simple_check(num: int):
-    for i in range(2, num):
-        if num % i == 0:
+def simple_check(g: int):
+    if g < 2:
+        return False
+    for i in range(2, g):
+        if g % i == 0:
             return False
-
     return True
 
-strt = int(input())
-list1 = list(range(strt+1))
+def generator():
+    n = 2
+    while True:
+        if simple_check(n):
+            yield n
+        n += 1
 
-list2 = [ x for x in list1 if simple_check(x)==True ]
-print(list2)
+def main():
+    num =int(input())
+    gen = generator()
+    for i in range(num+1):
+        print(next(gen))
+main()
